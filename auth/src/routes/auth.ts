9c391b2be547600cfd0300e046aa61253a23bb51
Dandy Zicky Divaldy <dandyzicky1@gmail.com>
2025-07-02 13:16:02 +0700

    chore(auth): preparing to create controllers and routes

---
+++ auth/src/routes/auth.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { Auth } from "../controllers/auth.js";
+
+export const router = Router();
+
+const auth = new Auth();
+
+router.post("/auth/login", (req, res) => {
+    auth.login(req, res);
+});
+
+router.post("/auth/logout", (req, res) => {
+    auth.logout(req, res);
+});
