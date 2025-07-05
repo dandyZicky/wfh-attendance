83ccc295a68c8aab05ddfbeebfd6335022d13204
Dandy Zicky Divaldy <dandyzicky1@gmail.com>
2025-07-05 12:34:06 +0700

    feat(auth)(user-management): finish user creation method

--- auth/src/services/auth.ts
+++ auth/src/services/auth.ts
@@ -9,7 +9,7 @@ export const findUserByEmail = async (email: string): Promise<Result<AuthUserEnt
     }
 
     const [rows] = await authDB.execute<RowDataPacket[]>(
-        "SELECT user_id, password_hash FROM users WHERE email = ?",
+        "SELECT user_key, email, username, password_hash FROM users WHERE email = ?",
         [email]
     );
     
@@ -21,3 +21,40 @@ export const findUserByEmail = async (email: string): Promise<Result<AuthUserEnt
     }
 
 }
+
+export const createUser = async (email: string, username: string, password_hash: string, user_key: string): Promise<Result<any, string>> => {
+    if (!email || !username || !password_hash || !user_key) {
+        return { success: false, error: "All fields are required" };
+    }
+
+    try {
+        const [result] = await authDB.execute(
+            "INSERT INTO users (user_key, email, username, password_hash) VALUES (?, ?, ?, ?)",
+            [user_key, email, username, password_hash]
+        );
+        return { success: true, data: result };
+    } catch (error: any) {
+        if (error.code === 'ER_DUP_ENTRY') {
+            return { success: false, error: "Username or email already exists" };
+        }
+        console.error("Error creating user in auth service:", error);
+        return { success: false, error: "DB error" };
+    }
+};
+
+export const deleteUser = async (user_key: string): Promise<Result<any, string>> => {
+    if (!user_key) {
+        return { success: false, error: "User Key is required" };
+    }
+
+    try {
+        const [result] = await authDB.execute(
+            "DELETE FROM users WHERE user_key = ?",
+            [user_key]
+        );
+        return { success: true, data: result };
+    } catch (error) {
+        console.error("Error deleting user in auth service:", error);
+        return { success: false, error: "DB error" };
+    }
+};
